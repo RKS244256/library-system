0aa4ade14430420b97ac71b1fe4c7a8204be988f
rks244256 <ryansirisena@gmail.com>
2024-04-30 13:03:32 -0400

    HTML and Javascript are like the women I like. I can't fix them and honestly, they're going to ruin me.

--- result.js
+++ result.js
@@ -1,13 +1,18 @@
 var bookNum;
 var bookTitle;
 var bookAuthor;
+var bookSearch;
+var bookTitleSearchMode;
 
 function displayResults(){
+    bookSearch = localStorage.getItem("bookSearch");
     bookNum = localStorage.getItem("bookIDs");
     bookTitle = localStorage.getItem("bookTitles");
     bookAuthor = localStorage.getItem("bookAuthors");
-    document.getElementById("bookIdResult").innerHTML = bookNum;
-    document.getElementById("bookTitleResult").innerHTML = bookTitle;
-    document.getElementById("bookAuthorResult").innerHTML = bookAuthor;
-    
+    console.log(bookSearch)
+    if(bookSearch.toLowerCase() === bookTitle.toLowerCase() && localStorage.getItem("bookSearchTitle")){
+        document.getElementById("bookIdResult").innerHTML = bookNum;
+        document.getElementById("bookTitleResult").innerHTML = bookTitle;
+        document.getElementById("bookAuthorResult").innerHTML = bookAuthor;
+    }
 }
