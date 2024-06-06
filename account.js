c4c498051992271e76009869ce43a4e94c126c65
rks244256 <ryansirisena@gmail.com>
2024-06-06 12:08:01 -0400

    If there's a null there's a goal

--- account.js
+++ account.js
@@ -1,9 +1,9 @@
 const sessionAccount = JSON.parse(sessionStorage.getItem("sessionAccount"))
 
 function checkSession(){
-    if(sessionAccount && !sessionAccount[3]){
+    if(sessionAccount && sessionAccount[3] === false){
         document.getElementById("accountNumber").innerHTML = `#${sessionAccount[0]}`
-    } else if(sessionAccount[3]){
+    } else if(sessionAccount && sessionAccount[3]){
         location.replace("library_technician_page.html")
     } else{
         location.replace("login.html");
